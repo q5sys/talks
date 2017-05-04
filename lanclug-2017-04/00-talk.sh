801f500059e84376955b0a9ea4aa2fa0e816217f
q5sys <jt@obs-sec.com>
2017-05-04 13:29:44 -0400

    add TrueOS talk

---
+++ lanclug-2017-04/00-talk.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+clear
+cat 01-intro.txt
+sed -n q </dev/tty
+clear
+cat 02-evolution.txt
+sed -n q </dev/tty
+clear
+cat 03-rolling.txt
+sed -n q </dev/tty
+clear
+cat 04-bootenvironments.txt
+sed -n q </dev/tty
+clear
+cat 05-divergence.txt
+sed -n q </dev/tty
+clear
+cat 06-testing.txt
+sed -n q </dev/tty
+clear
+cat 07-forging.txt
+sed -n q </dev/tty
+clear
+cat 08-forging-continued.txt
+sed -n q </dev/tty
+clear
+cat 09-pico.txt
+sed -n q </dev/tty
+clear
+cat 10-lumina.txt
+sed -n q </dev/tty
+clear
+cat 11-lumina-continued.txt
+sed -n q </dev/tty
+clear
+cat 12-ldifferences.txt
+sed -n q </dev/tty
+clear
+cat 13-future.txt
+sed -n q </dev/tty
+clear
+cat 14-closing.txt
+
